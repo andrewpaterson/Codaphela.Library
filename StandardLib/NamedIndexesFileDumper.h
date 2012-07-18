1aad44e2d7bcddbe30f9edd795960c51ad48daa3
Andrew Paterson <andrew.ian.paterson@gmail.com>
2012-07-18 21:54:37 +0200

    Added a 'dump' method for named indexes for debugging.

---
+++ StandardLib/NamedIndexesFileDumper.h
@@ -0,0 +1,9 @@
+#ifndef __NAMED_INDEXES_FILE_DUMPER_H__
+#define __NAMED_INDEXES_FILE_DUMPER_H__
+#include "BaseLib/Chars.h"
+
+
+BOOL DumpNamedIndexesFile(CChars* pszDest, char* szFileName, int iDataWidth, int iNumPerBlock);
+
+
+#endif // __NAMED_INDEXES_FILE_DUMPER_H__
