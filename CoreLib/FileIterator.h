7e8fcaa27621e60763b1e8ce9603162b3920cffd
Andrew Paterson <andrew.ian.paterson@gmail.com>
2012-08-12 09:25:04 +0200

    Spilt file FileIterator and FileIteratorReturn.

--- CoreLib/FileIterator.h
+++ CoreLib/FileIterator.h
@@ -1,38 +1,6 @@
 #ifndef __FILE_ITERATOR_H__
 #define __FILE_ITERATOR_H__
-#include "BaseLib/FileSystemIterator.h"
-#include "BaseLib/PackFileIterator.h"
-
-
-enum EFileIteratorReturnType
-{
-	FIRT_Unknown,
-	FIRT_FileSystem,
-	FIRT_PackFiles,
-};
-
-
-#define FILE_SYSTEM_RANK 8192
-
-
-class CFileIteratorReturn
-{
-protected:
-	EFileIteratorReturnType		meType;
-	CChars						mszFullName;
-	CBaseFileNode*				mpsNode;
-	int							miFileRank;
-
-public:
-	void						Init(void);
-	void						Init(EFileIteratorReturnType eType, CBaseFileNode* pcNode, int iFileRank);
-	void						Kill(void);
-
-	EFileIteratorReturnType		GetType(void);
-	char*						GetFullName(void);
-	CBaseFileNode*				GetNode(void);
-	int							GetFileRank(void);
-};
+#include "FileIteratorReturn.h"
 
 
 //This iterator gives back every file, including duplicates.
