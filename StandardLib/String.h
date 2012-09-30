63464af925afdce1aa266565293c4198248a9a62
Andrew Paterson <andrew.ian.paterson@gmail.com>
2012-09-30 22:07:40 +0200

    Updated String to KillData style over old Kill style.

--- StandardLib/String.h
+++ StandardLib/String.h
@@ -10,6 +10,7 @@ class CString : public CObject, public CChars
 BASE_FUNCTIONS(CString);
 public:
 	void	Kill(void);
+	void	KillData(void);
 
 	BOOL	Save(CObjectSerialiser* pcFile);
 	BOOL	Load(CObjectDeserialiser* pcFile);
