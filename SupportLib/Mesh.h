05ccbcd59911f18a293181f56426a10dafabab97
Andrew Paterson <andrew.ian.paterson@gmail.com>
2013-01-12 12:53:47 +0200

    Added list of objects needing to be changed from StandardTackerObjects to Objects.

--- SupportLib/Mesh.h
+++ SupportLib/Mesh.h
@@ -43,7 +43,7 @@ protected:
 
 public:
 	CMeshNormals		mcNormals;			
-	CMeshUVCoords		mcUVs;				
+	CMeshUVCoords		mcUVs;
 	CMeshMaterials		mcMaterials;		
 	CMeshColours		mcColours;			
 	CMeshSkin			mcSkin;				
