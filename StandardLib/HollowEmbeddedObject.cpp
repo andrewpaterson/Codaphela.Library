b01e0913a53b37bf0a8f9283eb0f04356692fb84
Andrew Paterson <andrew.ian.paterson@gmail.com>
2013-10-13 19:10:19 +0200

    Fixed HollowEmbeddedObject KillInternalData.

--- StandardLib/HollowEmbeddedObject.cpp
+++ StandardLib/HollowEmbeddedObject.cpp
@@ -112,7 +112,7 @@ int CHollowEmbeddedObject::GetEmbeddedIndex(void)
 //////////////////////////////////////////////////////////////////////////
 void CHollowEmbeddedObject::KillDontFree(void)
 {
-	KillFroms();
+	KillInternalData();
 }
 
 
