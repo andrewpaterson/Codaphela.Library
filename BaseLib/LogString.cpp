e9b01908eaf714a0587340b1ab5b59e1c9f65248
Andrew Paterson <andrew.ian.paterson@gmail.com>
2013-10-18 12:30:55 +0200

    Converted PointerToString to use CChars instead of string functions.

--- BaseLib/LogString.cpp
+++ BaseLib/LogString.cpp
@@ -63,7 +63,13 @@ char* PointerToString(void* pv)
 {
 	int iCount = IncrementLogToStringCount();
 	
-	IToA((size_t)pv, gaszLogToStringScratchPad[iCount], 16);
+	CChars	sz;
+
+	sz.Init();
+	sz.AppendHexHiLo(pv, sizeof(size_t));
+	//IToA((size_t)pv, gaszLogToStringScratchPad[iCount], 16);
+	strcpy(gaszLogToStringScratchPad[iCount], sz.Text());
+	sz.Kill();
 	return gaszLogToStringScratchPad[iCount];
 }
 
