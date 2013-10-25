238774fb421e2a799019fc1a6b1e4f72fb63aec5
Andrew Paterson <andrew.ian.paterson@gmail.com>
2013-10-25 18:11:52 +0200

    Added StackDistanceCalculator.  It doesn't do anything yet.

---
+++ StandardLib/DistToStackCalculator.h
@@ -0,0 +1,17 @@
+#ifndef __DIST_TO_STACK_CALCULATOR_H__
+#define __DIST_TO_STACK_CALCULATOR_H__
+#include "DistDetachedFroms.h"
+
+
+class CDistToStackCalculator
+{
+private:
+
+public:
+	void Init(void);
+	void Kill(void);
+};
+
+
+#endif // __DIST_TO_STACK_CALCULATOR_H__
+
