4c84626b700b8f1c76f1e33e7b5f63dc2c979034
Andrew Paterson <andrew.ian.paterson@gmail.com>
2014-08-03 07:58:21 +0200

    Added a memory allocator that passes through allocations but tracks the amount of memory allocated.

---
+++ BaseLib/TrackingAllocator.h
@@ -0,0 +1,30 @@
+#ifndef __TRACKING_ALLOCATOR_H__
+#define __TRACKING_ALLOCATOR_H__
+#include "Mallocator.h"
+#include "MapPtrPrimitiveTemplate.h"
+
+
+class CTrackingAllocator : public CMallocator
+{
+protected:
+	CMallocator*						mpcAlloc;
+	CMapPtrPrimitiveTemplate<size_t>	mmpiSizes;
+
+public:
+	void	Init(CMallocator* pcAlloc);
+	void	Kill(void);
+
+	size_t	AllocatedSize(void);
+
+	void*	Malloc(size_t tSize);
+	void*	Realloc(void* pv, size_t tSize);
+	void	Free(void* pv);
+
+	char*	GetName(void);
+
+	BOOL	IsLocal(void);
+};
+
+
+#endif // __TRACKING_ALLOCATOR_H__
+
