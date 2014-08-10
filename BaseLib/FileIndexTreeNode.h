a7dc581dcf22044aa7f968b9902770095f3821ea
Andrew Paterson <andrew.ian.paterson@gmail.com>
2014-08-10 20:26:57 +0200

    Added FileIndexTreeNode

---
+++ BaseLib/FileIndexTreeNode.h
@@ -0,0 +1,20 @@
+#ifndef __FILE_INDEX_TREE_NODE_H__
+#define __FILE_INDEX_TREE_NODE_H__
+#include "IndexTreeNode.h"
+#include "AbstractFile.h"
+
+
+class CFileIndexTreeNode : public CIndexTreeNode
+{
+protected:
+	int			miFileNumber;
+	filePos		miFileOffset;
+
+public:
+	void Init(void);
+};
+
+
+#endif // __FILE_INDEX_TREE_NODE_H__
+
+
