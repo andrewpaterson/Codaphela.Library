ffdfd3e05c0357e924b8dcf16eb64083867dab98
Andrew Paterson <andrew.ian.paterson@gmail.com>
2021-02-09 09:08:08 +0200

    Added Index and Data eviction counters.

---
+++ CoreLib/IndexedDataEvictionCounter.h
@@ -0,0 +1,21 @@
+#ifndef __INDEXED_DATA_EVICTION_COUNTER_H__
+#define __INDEXED_DATA_EVICTION_COUNTER_H__
+#include "BaseLib/PrimitiveTypes.h"
+#include "IndexedDataEvictionCallback.h"
+
+
+class CIndexedDataEvictionCounter : public CIndexedDataEvictionCallback
+{
+private:
+	int64	miEvictions;
+
+public:
+	void	Init(void);
+	void	Reset(void);
+	BOOL	IndexEvicted(OIndex oi, void* pvData, int iDataSize);
+	int64	EvictionCount(void);
+};
+
+
+#endif // __INDEXED_DATA_EVICTION_COUNTER_H__
+
