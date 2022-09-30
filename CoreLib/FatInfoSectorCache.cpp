3c427c25e95119317c26ecda4ffe37d3650f8481
Andrew Paterson <andrew.ian.paterson@gmail.com>
2022-09-30 11:27:07 +0200

    Still adding a FAT Sector cache.

--- CoreLib/FatInfoSectorCache.cpp
+++ CoreLib/FatInfoSectorCache.cpp
@@ -5,10 +5,38 @@
 //
 //
 //////////////////////////////////////////////////////////////////////////
-void CFatInfoSectorCache::Init(uint16 uiBytesPerSector)
+void CFatSectorCache::Init(uint32 uiInfoSector, uint64 uiCreationStamp)
 {
-	muiBytesPerSector = uiBytesPerSector;
+	muiInfoSector = uiInfoSector;
+	muiLocks = 0;
+	mbDirty = false;
+	muiCreationStamp = uiCreationStamp;
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+//
+//
+//////////////////////////////////////////////////////////////////////////
+void* CFatSectorCache::GetCache(void)
+{
+	return RemapSinglePointer(this, sizeof(CFatSectorCache));
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+//
+//
+//////////////////////////////////////////////////////////////////////////
+void CFatInfoSectorCache::Init(CFileDrive* pcDrive, uint16 uiMinimumUnlockedCaches)
+{
+	mpcDrive = pcDrive;
+	muiSectorSize = pcDrive->GetSectorSize();
+
 	mllcCachedSectors.Init();
+	muiMinimumUnlockedCaches = uiMinimumUnlockedCaches;
+	muiCaches = 0;
+	muiCreationStamp = 0;
 }
 
 
@@ -26,11 +54,131 @@ void CFatInfoSectorCache::Kill(void)
 //
 //
 //////////////////////////////////////////////////////////////////////////
-SFatSectorCache* CFatInfoSectorCache::AddCache(uint32 uiFatSharedBufferSector)
+CFatSectorCache* CFatInfoSectorCache::AddNewCache(void)
 {
-	SFatSectorCache*	psSectorCache;
+	CFatSectorCache*	pcSectorCache;
+	size_t				uiSize;
 
-	psSectorCache = (SFatSectorCache*)mllcCachedSectors.Add(sizeof(SFatSectorCache));
-	return psSectorCache;
+	uiSize = sizeof(CFatSectorCache) + muiSectorSize;
+	pcSectorCache = (CFatSectorCache*)mllcCachedSectors.Add(uiSize);
+
+	memset(pcSectorCache, 0, uiSize);
+	return pcSectorCache;
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+//
+//
+//////////////////////////////////////////////////////////////////////////
+CFatSectorCache* CFatInfoSectorCache::GetOrCacheSector(uint32 uiInfoSector)
+{
+	CFatSectorCache*	pcCachedSector;
+	CFatSectorCache*	pcEmptySector;
+	CFatSectorCache*	pcOldestUnlockedSector;
+	uint64				uiOldestStamp;
+	uint16				uiUnlockedCaches;
+	bool				bSuccess;
+
+	pcEmptySector = NULL;
+	pcOldestUnlockedSector = NULL;
+	uiOldestStamp = 0xFFFFFFFFFFFFFFFF;
+	uiUnlockedCaches = 0;
+	pcCachedSector = (CFatSectorCache*)mllcCachedSectors.GetHead();
+	while (pcCachedSector != NULL)
+	{
+		if (pcCachedSector->muiInfoSector == uiInfoSector)
+		{
+			return pcCachedSector;
+		}
+
+		if ((pcCachedSector->muiInfoSector == NO_SECTOR_CACHED) && (pcEmptySector == NULL))
+		{
+			pcEmptySector = pcCachedSector;
+		}
+
+		if (pcCachedSector->muiLocks == 0)
+		{
+			uiUnlockedCaches++;
+
+			if (pcCachedSector->muiCreationStamp < uiOldestStamp)
+			{
+				pcOldestUnlockedSector = pcCachedSector;
+				uiOldestStamp = pcCachedSector->muiCreationStamp;
+			}
+		}
+
+		pcCachedSector = (CFatSectorCache*)mllcCachedSectors.GetNext(pcCachedSector);
+	}
+
+	if (pcEmptySector)
+	{
+		return InitialiseCache(uiInfoSector, pcEmptySector);
+	}
+
+	if (uiUnlockedCaches < muiMinimumUnlockedCaches)
+	{
+		pcCachedSector = AddNewCache();
+		return InitialiseCache(uiInfoSector, pcCachedSector);
+	}
+
+	bSuccess = FlushCache(pcCachedSector);
+	if (!bSuccess)
+	{
+		return NULL;
+	}
+
+	return InitialiseCache(uiInfoSector, pcCachedSector);
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+//
+//
+//////////////////////////////////////////////////////////////////////////
+CFatSectorCache* CFatInfoSectorCache::InitialiseCache(uint32 uiInfoSector, CFatSectorCache* pcCachedSector)
+{
+	void*	pvData;
+	bool	bSuccess;
+
+	muiCreationStamp++;
+	pcCachedSector->Init(uiInfoSector, muiCreationStamp);
+	pvData = pcCachedSector->GetCache();
+	bSuccess = mpcDrive->Read(uiInfoSector, pvData);
+	if (!bSuccess)
+	{
+		return NULL;
+	}
+	else
+	{
+		return pcCachedSector;
+	}
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+//
+//
+//////////////////////////////////////////////////////////////////////////
+bool CFatInfoSectorCache::FlushCache(CFatSectorCache* pcCachedSector)
+{
+	void*	pvData;
+	bool	bSuccess;
+
+	if (pcCachedSector->mbDirty)
+	{
+		pvData = pcCachedSector->GetCache();
+		bSuccess = mpcDrive->Write(pcCachedSector->muiInfoSector, pvData);
+		if (!bSuccess)
+		{
+			return false;
+		}
+		else
+		{
+			pcCachedSector->mbDirty = false;
+			return true;
+		}
+	}
+	return true;
 }
 
--- CoreLib/FatInfoSectorCache.h
+++ CoreLib/FatInfoSectorCache.h
@@ -2,13 +2,22 @@
 #define __FAT_INFO_SECTOR_CACHE__
 #include "BaseLib/PrimitiveTypes.h"
 #include "BaseLib/LinkedListBlock.h"
+#include "FileDrive.h"
 
 
-struct SFatSectorCache
+#define	NO_SECTOR_CACHED	0xffffffff
+
+
+class CFatSectorCache
 {
-	uint32	uiFatSharedBufferSector;
-	uint16	uiLocks;
-	bool	bDirty;
+public:
+	uint64	muiCreationStamp;
+	uint32	muiInfoSector;
+	uint16	muiLocks;
+	bool	mbDirty;
+
+	void	Init(uint32 uiInfoSector, uint64 uiCreationStamp);
+	void*	GetCache(void);
 };
 
 
@@ -16,14 +25,24 @@ class CFatInfoSectorCache
 {
 protected:
 	CLinkedListBlock	mllcCachedSectors;
-	uint16				muiBytesPerSector;
+	uint16				muiSectorSize;
+	uint16				muiMinimumUnlockedCaches;
+	uint16				muiCaches;
+
+	CFileDrive*			mpcDrive;
+	uint64				muiCreationStamp;
 
 public:
-	void				Init(uint16 uiBytesPerSector);
+	void				Init(CFileDrive* pcDrive, uint16 uiMinimumUnlockedCaches);
 	void				Kill(void);
 
-	SFatSectorCache* AddCache(uint32 uiFatSharedBufferSector);
+protected:
+	CFatSectorCache*	AddNewCache(void);
+	CFatSectorCache*	GetOrCacheSector(uint32 uiInfoSector);
+	CFatSectorCache*	InitialiseCache(uint32 uiInfoSector, CFatSectorCache* pcCachedSector);
+	bool				FlushCache(CFatSectorCache* pcCachedSector);
 };
 
 
 #endif // __FAT_INFO_SECTOR_CACHE__
+
