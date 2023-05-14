919fa612d3fdeebd8dfec2e4a4bfd6e1ce86cd85
Andrew Paterson <andrew.ian.paterson@gmail.com>
2023-05-14 12:37:27 +0200

    Renamed CoreLib strings to BaseLib.

--- BaseLib/CalcParentheses.h
+++ BaseLib/CalcParentheses.h
@@ -2,20 +2,20 @@
 
 Copyright (c) 2022 Andrew Paterson
 
-This file is part of The Codaphela Project: Codaphela CoreLib
+This file is part of The Codaphela Project: Codaphela BaseLib
 
-Codaphela CoreLib is free software: you can redistribute it and/or modify
+Codaphela BaseLib is free software: you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-Codaphela CoreLib is distributed in the hope that it will be useful,
+Codaphela BaseLib is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU Lesser General Public License for more details.
 
 You should have received a copy of the GNU Lesser General Public License
-along with Codaphela CoreLib.  If not, see <http://www.gnu.org/licenses/>.
+along with Codaphela BaseLib.  If not, see <http://www.gnu.org/licenses/>.
 
 Microsoft Windows is Copyright Microsoft Corporation
 
